b2e50090614acd4f6a51ee06e9423ff19f1505fc
amit-s-k <amitsk2014@gmail.com>
2019-01-15 17:42:46 +0530

    Task -4 Solution

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,6 +1,13 @@
 package com.stackroute.domain;
 
-public class Movie {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
     private Actor actor;
 
     public Movie() {
@@ -12,7 +19,7 @@ public class Movie {
     }
 
     public void log() {
-        System.out.println("Movie begins in 3 2 1 ...");
+        System.out.println(actor.toString());
     }
 
     public Actor getActor() {
@@ -21,5 +28,19 @@ public class Movie {
 
     public void setActor(Actor actor) {
         this.actor = actor;
+    }
+
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        Movie movie = (Movie) beanFactory.getBean("movieA");
+        movie.log();
+    }
+
+    public void setBeanName(String name) {
+        System.out.println("Bean Name is " + name);
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        Movie movie = (Movie) applicationContext.getBean("movieA");
+        movie.log();
     }
 }
