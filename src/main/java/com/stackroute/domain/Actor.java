335277745e3eb5fd70a32f24801e60def2ff24e7
amit-s-k <amitsk2014@gmail.com>
2019-01-15 17:26:56 +0530

    Task -3 Solution

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -38,4 +38,9 @@ public class Actor {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public String toString(){
+        return "Actor Name: "+this.name+"\nActor Age "+this.age+"\nActor gender "+this.gender;
+    }
 }
